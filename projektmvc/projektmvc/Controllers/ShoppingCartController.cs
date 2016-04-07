b8bf10c18c08f724e26561f4904d535f452a42ba
Karin Jonsson <jonsson.karin.ae@gmail.com>
2016-04-07 14:43:28 +0200

    Added skeleton for shopping cart

--- projektmvc/projektmvc/Controllers/ShoppingCartController.cs
+++ projektmvc/projektmvc/Controllers/ShoppingCartController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using projektmvc.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,10 +15,59 @@ namespace projektmvc.Controllers
             return View();
         }
 
-        public ActionResult AddToCart()
+        public ActionResult AddToCart(string productId, string productName, string productPrice)
         {
+            List<ProductModel> products = (List<ProductModel>)Session["Products"];
+            List<OrderItem> orderedItems = new List<OrderItem>();
+
+            int id = int.Parse(productId);
+            int price = int.Parse(productPrice);
+            bool inCart = false;
+            int NumInStock = 0;
+
+            //Kolla om varan finns i lager
+            foreach (ProductModel m in products)
+            {
+                if (m.Id == id)
+                {
+                    NumInStock = (m.NumberInStock > 0) ? m.NumberInStock : 0;
+                  
+                }
+            }
+
+            //Kolla om id:t redan finns i ordern
+            foreach (OrderItem item in orderedItems)
+            {
+                if (item.Id == id)
+                {
+                    inCart = true;
+                    break;
+                }
+            }
+
+            if (!inCart)
+            {
+                orderedItems.Add(new OrderItem(id, productName, price, 1));
+                Session["Cart"] = orderedItems;
+            }
+
+
+            //OrderItem itemToBuy = new OrderItem(id, productName, price, 1);
+
             //Kolla mot Session["Cart"], den får inte vara null
-            return View();
+            Session["Cart"] = orderedItems;
+
+            
+            if (Session["Cart"] != null)
+            {
+
+
+            }
+
+
+            return View("ShoppingCart");  //"Products", products
+
         }
     }
 }
+
---
+++ projektmvc/projektmvc/Models/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projektmvc.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public int Price { get; private set; }
+
+        public int Numbers { get; private set; }
+
+        public OrderItem(int id, string name, int price, int number)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Numbers = number;
+        }
+    }
+}
