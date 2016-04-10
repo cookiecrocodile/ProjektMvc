681affe8c7cec658f4ffc09e912021e4f3257a3f
Omran <palman115@hotmail.com>
2016-04-10 19:59:38 +0200

    Good Jobb!!

--- projektmvc/projektmvc/Controllers/ShoppingCartController.cs
+++ projektmvc/projektmvc/Controllers/ShoppingCartController.cs
@@ -9,68 +9,212 @@ namespace projektmvc.Controllers
 {
     public class ShoppingCartController : Controller
     {
-        // GET: ShoppingCart
-        public ActionResult ShoppingCart()
-        {
+		List<OrderItem> orderedItems = new List<OrderItem>();
+
+		// GET: ShoppingCart
+		public ActionResult ShoppingCart()
+		{
+			if (Session["Cart"] != null)
+			{
+				orderedItems = (List<OrderItem>)Session["Cart"];
+			}
+
+			//Lägg till logik för att räkna ut siffrorna
+			object itTot = OrderItem.CalculateNumberOfItems(orderedItems);
+			object prTot = OrderItem.CalculateTotal(orderedItems);
+
+			if (itTot == null || prTot == null)
+			{
+				ViewBag.ItemsTotal = 0;
+				ViewBag.PriceTotal = 0;
+			}
+			else
+			{
+				ViewBag.ItemsTotal = itTot;
+				ViewBag.PriceTotal = prTot;
+			}
+
+			return View(orderedItems);
+		}
+
+		//Add product to cart
+		public ActionResult AddToCart(string productId, string productName, string productPrice)
+		{
 			List<ProductModel> products = (List<ProductModel>)Session["Products"];
-			List<OrderItem> orderedItems = new List<OrderItem>();
+
+			if (Session["Cart"] != null)
+			{
+				orderedItems = (List<OrderItem>)Session["Cart"];
+			}
+
+			int id = int.Parse(productId);
+			int price = int.Parse(productPrice);
+			int NumInStock = 0;
+			bool inCart = false;
+
+			//Kolla om varan finns i lager
+			foreach (ProductModel m in products)
+			{
+				if (m.Id == id)
+				{
+					//Kolla i produktlistan hur många som finns i lager, spara i variabel
+					NumInStock = (m.NumberInStock > 0) ? m.NumberInStock : 0;
+					break;
+				}
+			}
+
+			//Kolla om en produkt med samma id redan finns i ordern
+			foreach (OrderItem item in orderedItems)
+			{
+				if (item.Id == id)
+				{
+					inCart = true;
+					//TEST
+					if (NumInStock >= item.Numbers + 1)//Om antalet i lager är större än eller lika med det antal man har +1
+					{
+						OrderItem.EditNumberOfItems(item, item.Numbers + 1);
+					}
+					Session["Cart"] = orderedItems;
+				}
+
+				//break;
+			}
+
+			if (!inCart)
+			{
+				if (NumInStock >= 1)//Om antalet i lager är större än eller lika med 1
+				{
+					orderedItems.Add(new OrderItem(id, productName, price, 1));
+				}
+				Session["Cart"] = orderedItems;
+			}
+
+			orderedItems = (List<OrderItem>)Session["Cart"];
+
+			//Lägg till logik för att räkna ut siffrorna
+			object itTot = OrderItem.CalculateNumberOfItems(orderedItems);
+			object prTot = OrderItem.CalculateTotal(orderedItems);
+
+			if (itTot == null || prTot == null)
+			{
+				ViewBag.ItemsTotal = 0;
+				ViewBag.PriceTotal = 0;
+			}
+			else
+			{
+				ViewBag.ItemsTotal = (int)itTot;
+				ViewBag.PriceTotal = (int)prTot;
+			}
+
+			return View("../Product/Products", products);
+
+			//return View("ShoppingCart", orderedItems);
+		}
+
+		//Actionen som tar bort objekt ur ordern
+		public ActionResult Remove(string id)
+		{
+			List<OrderItem> orderedItems = (List<OrderItem>)Session["Cart"];
+			int prodId;
+			int.TryParse(id, out prodId);
+
+			foreach (OrderItem item in orderedItems)
+			{
+				if (item.Id == prodId)
+				{
+					orderedItems.Remove(item);
+					break; //avbryt när den har gjort det en gång
+				}
+			}
+
+			Session["Cart"] = orderedItems;
+
+			//Lägg till logik för att räkna ut siffrorna
+			object itTot = OrderItem.CalculateNumberOfItems(orderedItems);
+			object prTot = OrderItem.CalculateTotal(orderedItems);
+
+			if (itTot == null || prTot == null)
+			{
+				ViewBag.ItemsTotal = 0;
+				ViewBag.PriceTotal = 0;
+			}
+			else
+			{
+				ViewBag.ItemsTotal = (int)itTot;
+				ViewBag.PriceTotal = (int)prTot;
+			}
+
+			return View("ShoppingCart", orderedItems); //Skickar tillbaka vyn Products med modellen products
+
+		}
+
+		public ActionResult Edit()
+		{
+			List<OrderItem> orderedItems = (List<OrderItem>)Session["Cart"];
+			List<ProductModel> products = (List<ProductModel>)Session["Products"];
+
+			string num = Request.Form["amount"];
+			string id = Request.Form["pId"];
+
+			int pId = int.Parse(id);
+			int amount = int.Parse(num);
+
+			int NumInStock = 0;
+
+			//Kolla om varan finns i lager
+			foreach (ProductModel m in products)
+			{
+				if (m.Id == pId)
+				{
+					//Kolla i produktlistan hur många som finns i lager, spara i variabel
+					NumInStock = (m.NumberInStock > 0) ? m.NumberInStock : 0;
+					break;
+				}
+			}
+
+			//Ändra antal
+			foreach (OrderItem item in orderedItems)
+			{
+				if (pId == item.Id)
+				{
+					if (NumInStock >= amount)
+					{
+						OrderItem.EditNumberOfItems(item, amount);
+						break;
+					}
+					else
+					{
+						//Gör något för att visa att det inte gick
+					}
+				}
+			}
+
+			Session["Cart"] = orderedItems;
+
+			//Logik för att räkna ut siffrorna
+			object itTot = OrderItem.CalculateNumberOfItems(orderedItems);
+			object prTot = OrderItem.CalculateTotal(orderedItems);
+
+			if (itTot == null || prTot == null)
+			{
+				ViewBag.ItemsTotal = 0;
+				ViewBag.PriceTotal = 0;
+			}
+			else
+			{
+				ViewBag.ItemsTotal = (int)itTot;
+				ViewBag.PriceTotal = (int)prTot;
+			}
+
+			return View("ShoppingCart", orderedItems);
+		}
+
+		public ActionResult Send()
+		{
+			Session["Cart"] = null;
 
 			return View();
-        }
-
-        public ActionResult AddToCart(string productId, string productName, string productPrice)
-        {
-            List<ProductModel> products = (List<ProductModel>)Session["Products"];
-            List<OrderItem> orderedItems = new List<OrderItem>();
-
-            int id = int.Parse(productId);
-            int price = int.Parse(productPrice);
-            bool inCart = false;
-            int NumInStock = 0;
-
-            //Kolla om varan finns i lager
-            foreach (ProductModel m in products)
-            {
-                if (m.Id == id)
-                {
-                    NumInStock = (m.NumberInStock > 0) ? m.NumberInStock : 0;
-                  
-                }
-            }
-
-            //Kolla om id:t redan finns i ordern
-            foreach (OrderItem item in orderedItems)
-            {
-                if (item.Id == id)
-                {
-                    inCart = true;
-                    break;
-                }
-            }
-
-            if (!inCart)
-            {
-                orderedItems.Add(new OrderItem(id, productName, price, 1));
-                Session["Cart"] = orderedItems;
-            }
-
-
-            //OrderItem itemToBuy = new OrderItem(id, productName, price, 1);
-
-            //Kolla mot Session["Cart"], den får inte vara null
-            Session["Cart"] = orderedItems;
-
-            
-            if (Session["Cart"] != null)
-            {
-
-
-            }
-
-
-            return View("ShoppingCart");  //"Products", products
-
-        }
-    }
+		}
+	}
 }
 
--- projektmvc/projektmvc/Models/OrderItem.cs
+++ projektmvc/projektmvc/Models/OrderItem.cs
@@ -7,19 +7,49 @@ namespace projektmvc.Models
 {
     public class OrderItem
     {
-        public int Id { get; private set; }
-        public string Name { get; private set; }
+		public int Id { get; private set; }
+		public string Name { get; private set; }
 
-        public int Price { get; private set; }
+		public int Price { get; private set; }
 
-        public int Numbers { get; private set; }
+		public int Numbers { get; private set; }
 
-        public OrderItem(int id, string name, int price, int number)
-        {
-            Id = id;
-            Name = name;
-            Price = price;
-            Numbers = number;
-        }
-    }
+		public OrderItem(int id, string name, int price, int number)
+		{
+			Id = id;
+			Name = name;
+			Price = price;
+			Numbers = number;
+		}
+
+		//Sätter om propertyn number på ett inskickat OrderItem till det antal man anger
+		public static void EditNumberOfItems(OrderItem item, int num)
+		{
+			item.Numbers = num;
+		}
+
+		public static int CalculateNumberOfItems(List<OrderItem> order)
+		{
+			int itemTotal = 0;
+
+			foreach (OrderItem item in order)
+			{
+				itemTotal += item.Numbers;
+			}
+
+			return itemTotal;
+		}
+
+		public static int CalculateTotal(List<OrderItem> order)
+		{
+			int total = 0;
+
+			foreach (OrderItem item in order)
+			{
+				total += (item.Numbers * item.Price);
+			}
+
+			return total;
+		}
+	}
 }
